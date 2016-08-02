9ed40fd569cdf9eb4978b2f15065c1c0c373665a
Kirmayr <tomaz.kirmayr@gmail.com>
2016-08-02 12:10:26 -0400

    Inserindo primeiro commit da api descubra manaus

---
+++ api_project.js
@@ -0,0 +1,19 @@
+define({
+  "name": "Conheço Manaus",
+  "version": "1.0.0",
+  "description": "Api conheça Manaus",
+  "apidoc": "0.2.0",
+  "header": {
+    "title": "Conheça manaus"
+  },
+  "footer": {
+    "title": "Conheça Manaus"
+  },
+  "sampleUrl": false,
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-08-02T16:09:43.690Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
